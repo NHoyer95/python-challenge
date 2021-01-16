19e2e37a40e5241817303d26027a0c9c5018d959
NHoyer95 <hoyer047@umn.edu>
2021-01-16 16:59:23 -0600

    Update the PyBank Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,20 +7,41 @@ import csv
 # Find the file
 pybank_csv = os.path.join('Resources', 'PyBank_FinancialData.csv')
 
-# Read in the CSV
+bank_data = []
 
+# Define the function and have it accept 'bank_data' as its sole parameter
+def print_bank_data(bank_data):
+    
+    # Assign values to usefully named variables
+    ProfLoss = 0
+    
+    for months in bank_data:
+        ProfLoss += int(months[1])
+
+    print(ProfLoss)
+    # set total months
+    total_months = len(bank_data)
+
+    print(total_months)
+
+
+# Read in the CSV
 with open(pybank_csv, 'r') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
     # Read the header row 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+    
+        bank_data.append(row)
+
+    # Print required output
+    print(f"Financial Analysis")
+    print(f"--------------------------")    
+    # print(f"Total months: {total_months}")
+    print_bank_data(bank_data)
 
